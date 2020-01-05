7b953e3e26e9a86aebdcb1a7fc9a6ec542402131
Imam Hossain Roni <imamhossainroni95@gmail.com>
2020-01-05 21:58:40 +0600

    >>APP ADD <blog> custom model manager created.

--- blog/models.py
+++ blog/models.py
@@ -1,9 +1,13 @@
 from django.contrib.auth.models import User
 from django.db import models
 
-
 # Create your models here.
 from django.utils import timezone
+
+
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super(PublishedManager, self).get_queryset().filter(status='published')
 
 
 class Post(models.Model):
@@ -19,9 +23,11 @@ class Post(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')
+    objects = models.Manager()
+    published = PublishedManager()
 
     class Meta:
-        ordering = ('-publish', )
+        ordering = ('-publish',)
 
     def __str__(self):
         return self.title
