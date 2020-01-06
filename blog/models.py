0aee6c0c5b70927e0a712e5d10e28dedd8d73c37
Imam Hossain Roni <imamhossainroni95@gmail.com>
2020-01-06 22:33:02 +0600

    >>APP ADD <blog> post list page done.

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
 from django.utils import timezone
@@ -31,3 +32,10 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('post_details',
+                       args=[self.publish.year,
+                             self.publish.month,
+                             self.publish.day,
+                             self.slug])
