515becb37c8f726f968905ba8a386377c3cd08ce
Imam Hossain Roni <imamhossainroni95@gmail.com>
2020-01-08 21:33:59 +0600

    >>APP ADD <blog> Pagination added in home page.

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,21 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.generic import ListView
+
 # Create your views here.
 
 
 def post_list(request):
-    posts = Post.published.all()
+    object_list = Post.published.all()
+    page = request.GET.get('page')
+    paginator = Paginator(object_list, 3)
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
     return render(request, 'list.html', {'posts': posts})
 
 
