a1f01862bb220cfc86f36a7d5ed26bd0d6ca8ecd
Imam Hossain Roni <imamhossainroni95@gmail.com>
2021-04-28 01:48:01 +0600

    allowed host added

--- dj_blog/settings.py
+++ dj_blog/settings.py
@@ -116,9 +116,10 @@ USE_TZ = True
 # STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
-STATIC_ROOT = '/root/webapps/dj_blog/static_assets/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+STATIC_ROOT = os.path.join(BASE_DIR, "../static/")
